65868cef982a804906d7a82a15915649fa1aecb1
Crowdbotics <team@crowdbotics.com>
2022-11-13 01:49:58 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,10 +1,15 @@
-const styles = StyleSheet.create({});
-import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({})
+import { StyleSheet } from "react-native"
 export const globalOptions = {
-  "name": "driver_37574",
-  "url": "https://driver_37574.botics.co",
-  "api": "https://driver_37574.botics.co/api/v1"
-};
+  name: "driver_37574",
+  url: "https://driver_37574.botics.co",
+  api: "https://driver_37574.botics.co/api/v1"
+}
+
 export const modulesOptions = {
-  "@modules/app-menu": {}
-};
+  "@modules/app-menu": {
+    title: "App Menu",
+    copy: "Routes available",
+    styles: styles
+  }
+}
